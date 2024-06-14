ccf6dc276082ec78e115e144764a856e31e616ca
xeim <xeimdonot@gmail.com>
2024-06-14 23:18:13 +0300

    added rectifier function in math module

---
+++ detector/sources/math/rectifier.cpp
@@ -0,0 +1,5 @@
+#include "rectifier.hpp"
+
+double dod::math::rectifier(const double x) {
+    return x>0 ? x : 0;
+}
---
+++ detector/sources/math/rectifier.hpp
@@ -0,0 +1,8 @@
+#ifndef DETECTOROFDIGITS_RECTIFIER_HPP
+#define DETECTOROFDIGITS_RECTIFIER_HPP
+
+namespace dod::math {
+    double rectifier(double x);
+}
+
+#endif //DETECTOROFDIGITS_RECTIFIER_HPP
