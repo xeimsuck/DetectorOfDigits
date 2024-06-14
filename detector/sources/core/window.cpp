b9a26b2b6ace8a473607e21126f2fccbdcfd6130
xeim <xeimdonot@gmail.com>
2024-06-15 01:43:23 +0300

    add all gui elements into window class and constructor their

--- detector/sources/core/window.cpp
+++ detector/sources/core/window.cpp
@@ -1,5 +1,47 @@
 #include "window.hpp"
 
-dod::core::window::window() {
-    this->setMinimumSize(WIDTH, HEIGHT);
+using std::make_unique;
+
+dod::core::window::window(){
+    this->setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    this->setWindowTitle(WINDOW_TITLE);
+
+    /*=============== WINDOW STUFF ===============*/
+    centralWidget = make_unique<QWidget>();
+    this->setCentralWidget(centralWidget.get());
+
+    mainLayout = make_unique<QGridLayout>();
+    centralWidget->setLayout(mainLayout.get());
+
+    /*=============== INPUT GROUP ===============*/
+    inputGroup = make_unique<QGroupBox>();
+    inputGroupLayout = make_unique<QVBoxLayout>();
+    inputGroup->setLayout(inputGroupLayout.get());
+    mainLayout->addWidget(inputGroup.get());
+
+    QSize q_sizeInputField(DRAW_WIDGET_WIDTH, DRAW_WIDGET_HEIGHT);
+    inputField = std::make_unique<gui::drawWidget>(q_sizeInputField , this);
+    inputGroupLayout->addWidget(inputField.get());
+
+    clearButton = make_unique<QPushButton>("Clear");
+    QObject::connect(clearButton.get(), &QPushButton::clicked, inputField.get(), &gui::drawWidget::clear);
+    inputGroupLayout->addWidget(clearButton.get());
+
+    /*=============== OUTPUT GROUP ===============*/
+    outputGroupLayout = make_unique<QVBoxLayout>();
+    outputGroup = make_unique<QGroupBox>();
+    outputGroup->setLayout(outputGroupLayout.get());
+    mainLayout->addWidget(outputGroup.get(), 1, 0);
+
+    finalResultLabel = make_unique<QLabel>("-");
+    finalResultLabel->setFont(QFont("Arial", 35, QFont::Bold));
+    finalResultLabel->setAlignment(Qt::AlignCenter);
+    outputGroupLayout->addWidget(finalResultLabel.get());
+
+    allResultsLayout = make_unique<QHBoxLayout>();
+    outputGroupLayout->addLayout(allResultsLayout.get());
+    for(size_t i = 0; i<10; ++i){
+        allResultsLabels[i] = make_unique<gui::resultWidget>(QString::number(i));
+        allResultsLayout->addWidget(allResultsLabels[i].get());
+    }
 }
--- detector/sources/core/window.hpp
+++ detector/sources/core/window.hpp
@@ -1,16 +1,42 @@
 #ifndef DOD_APPLICATION
 #define DOD_APPLICATION
 
+#include <QObject>
 #include <QMainWindow>
+#include <QGroupBox>
+#include "gui/drawWidget.hpp"
+#include "gui/resultWidget.hpp"
+#include "neuralNetwork.hpp"
 
-inline constexpr char TITLE[] = "Detector Of Digits";
-inline constexpr unsigned WIDTH = 600;
-inline constexpr unsigned HEIGHT = 800;
+inline constexpr char WINDOW_TITLE[] = "Detector Of Digits";
+inline constexpr unsigned WINDOW_WIDTH = 600;
+inline constexpr unsigned WINDOW_HEIGHT = 800;
+inline constexpr unsigned DRAW_WIDGET_WIDTH = WINDOW_WIDTH/100*90;
+inline constexpr unsigned DRAW_WIDGET_HEIGHT = WINDOW_HEIGHT/100*60;
+
 
 namespace dod::core {
     class window final : public QMainWindow {
     public:
         window();
+    private:
+        // Window Stuff
+        std::unique_ptr<QWidget> centralWidget = nullptr;
+        std::unique_ptr<QGridLayout> mainLayout = nullptr;
+
+        // Group "Input"
+        std::unique_ptr<QGroupBox> inputGroup = nullptr;
+        std::unique_ptr<QVBoxLayout> inputGroupLayout = nullptr;
+        std::unique_ptr<gui::drawWidget> inputField = nullptr;
+        std::unique_ptr<QPushButton> clearButton = nullptr;
+
+        // Group "Result"
+        std::unique_ptr<QGroupBox> outputGroup = nullptr;
+        std::unique_ptr<QVBoxLayout> outputGroupLayout = nullptr;
+        std::unique_ptr<QLabel> finalResultLabel = nullptr;
+
+        std::unique_ptr<gui::resultWidget> allResultsLabels[10];
+        std::unique_ptr<QHBoxLayout> allResultsLayout = nullptr;
     };
 }
 
