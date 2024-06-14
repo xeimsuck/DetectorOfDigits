acd9005f9dcdd6b1a857823794fbbad5a2f17d45
xeim <xeimdonot@gmail.com>
2024-06-15 01:11:43 +0300

    created drawWidget class

---
+++ detector/sources/gui/drawWidget.cpp
@@ -0,0 +1,28 @@
+#include "drawWidget.hpp"
+
+dod::gui::drawWidget::drawWidget(QSize size, QWidget *parent) : QWidget(parent) {
+    this->setFixedSize(size);
+}
+
+void dod::gui::drawWidget::paintEvent(QPaintEvent *event) {
+    QPainter painter(this);
+    painter.setPen(QPen(QBrush(Qt::black), 10));
+    for(auto& pl : v_plPoints) painter.drawPolyline(pl); //or drawPoints
+}
+
+void dod::gui::drawWidget::addPoint(QPoint point) {
+    v_plPoints.back().push_back(point);
+    update();
+}
+
+void dod::gui::drawWidget::clear() {
+    v_plPoints.clear();
+    update();
+}
+
+void dod::gui::drawWidget::mousePressEvent(QMouseEvent* event) {
+    v_plPoints.emplace_back();
+}
+void dod::gui::drawWidget::mouseMoveEvent(QMouseEvent* event) {
+    addPoint(event->pos());
+}
---
+++ detector/sources/gui/drawWidget.hpp
@@ -0,0 +1,29 @@
+#ifndef DOD_DRAWWIDGET
+#define DOD_DRAWWIDGET
+
+#include <vector>
+#include <memory>
+
+#include <QMouseEvent>
+#include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QPainter>
+#include <QPolygon>
+
+namespace dod::gui {
+    class drawWidget final : public QWidget {
+    public:
+        explicit drawWidget(QSize size, QWidget* parent = nullptr);
+        void paintEvent(QPaintEvent* event) override;
+        void addPoint(QPoint point);
+        void mousePressEvent(QMouseEvent*) final;
+        void mouseMoveEvent(QMouseEvent*) final;
+        void clear();
+
+    private:
+        std::vector<QPolygon> v_plPoints;
+    };
+}
+
+#endif //DOD_DRAWWIDGET
